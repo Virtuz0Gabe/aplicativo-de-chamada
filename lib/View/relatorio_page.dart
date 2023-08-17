68b204623fae1b53731f8a22412162f34c0b4b38
Gabriel W.K <gabrielwernerkuhn1@gmail.com>
2023-08-17 10:48:45 -0300

    finalização da impressão do pdf

--- lib/View/relatorio_page.dart
+++ lib/View/relatorio_page.dart
@@ -1,5 +1,11 @@
-import "package:flutter/material.dart";
+import 'dart:io';
+import 'package:flutter/material.dart';
 import 'package:chamada/model/aluno.dart';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:path_provider/path_provider.dart';
+import 'package:intl/intl.dart';
+
 
 class RelatorioPage extends StatefulWidget {
   List<Aluno> chamada;
@@ -10,6 +16,7 @@ class RelatorioPage extends StatefulWidget {
 }
 
 class _RelatorioPageState extends State<RelatorioPage> {
+  final pdf = pw.Document();
 
   @override
   Widget build(BuildContext context) {
@@ -24,6 +31,14 @@ class _RelatorioPageState extends State<RelatorioPage> {
           onPressed: () {Navigator.pop(context);
           },
         ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.print),
+            onPressed: () {
+              salvarPdf(widget.chamada);
+            }
+          )
+        ],
       ),
       body: ListView.builder(
         itemCount: widget.chamada.length,
@@ -57,4 +72,72 @@ class _RelatorioPageState extends State<RelatorioPage> {
       )
     );
   }
-}
+
+
+
+  Future<void> salvarPdf(List<Aluno> chamada) async {
+    gerarPdf(chamada);
+
+    final Directory appDocDir = await getApplicationDocumentsDirectory();  
+    final Directory pdfDir = Directory('/storage/emulated/0/Download/pdfs');
+    pdfDir.createSync(recursive: true);
+
+    final String pdfPath = '${pdfDir.path}/relatorio.pdf';
+    final File pdfFile = File(pdfPath);
+    pdfFile.writeAsBytesSync( await pdf.save());
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(content: Text("Pdf Salvo com sucesso em: $pdfPath")),
+    );
+  }
+    
+   void gerarPdf (List<Aluno> chamada) {
+    pdf.addPage(pw.Page(
+      pageFormat: PdfPageFormat.a4,
+      build: (pw.Context context) {
+        return pw.ListView(
+          children: [
+            pw.Center(
+              child: 
+                pw.Container(
+                  alignment: pw.Alignment.center,
+                  padding: const pw.EdgeInsets.all(16),
+                  child: pw.Column(
+                    children: [
+                      pw.Text(
+                        "Relatório de presença",
+                        style: pw.TextStyle (
+                          fontSize: 20,
+                          fontWeight: pw.FontWeight.bold),
+                        ),
+                        pw.Text (
+                        "Data: ${DateFormat("dd/MM/yyyy HH:mm").format(DateTime.now())}",
+                        style: const pw.TextStyle(fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              pw.Divider(),
+              buildList(chamada),
+            ],
+          );
+        },
+    ));
+   }
+
+    pw.Widget buildList(List<Aluno> chamada) {
+      final alunosWidgets = widget.chamada.map((aluno) {
+        return pw.Row(
+          mainAxisAlignment: pw.MainAxisAlignment.spaceBetween,
+          children: [
+            pw.Text("${aluno.nome} ${aluno.sobrenome}"),
+            pw.Text(aluno.presente ? "Presente" : "Ausente"),
+          ],
+        );
+      }).toList();
+
+      return pw.Column(children: alunosWidgets);
+    }
+  }
+
